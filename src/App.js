88f2146c95e50142e760c7ae57438a55c65ff45e
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-12-04 01:05:24 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   // const closeModal = () => setIsOpen(false);
 
