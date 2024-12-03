57742025419ef699e16f87167f1d4dc205d6ca0b
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-12-04 01:55:28 +0400

    update button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+
+  const closeModal = () => {
+    const parentDivId = window.frameElement?.parentElement?.id;
+
+    console.log(parentDivId, 'parentDivId'); 
+  }
+
   return (
     <>
       <div id='W2W_MODAL' className="modal">
         <div className="modal-dialog">
           <div className="modal-header">
             <h2>Modal in CSS?</h2>
-            <button id='W2W_CLOSE' className="btn-close" aria-label="Close">×</button>
+            <button id='W2W_CLOSE' className="btn-close" onClick={closeModal} aria-label="Close">×</button>
           </div>
           <div className="modal-body">
             <p>One modal example here! :D</p>
