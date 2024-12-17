1763e31fec6a0c2595d9f8e28b01342a2d2b9373
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-12-18 03:02:34 +0400

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
     <>
       <div id='W2W_MODAL' className="modal">
         <div className="modal-dialog">
-          <div className="modal-header">
-            <h2>Wearify</h2>
-          </div>
           <div className="modal-body">
             <iframe
               src="https://style.clo-set.com/embed/2b0aae6007514254a9cca9f34296180b/1/c/0?bgColor=%23f5f5f5&ui=0&colorway=0&logo=none"
